ec775d375beecde87ec38eec865aa24da2785ab7
Svetlana Bahlay <conbka@gmail.com>
2021-05-18 22:18:27 +0300

    added panel component and added settings for template list

--- src/app/components/template-details/template-details.component.ts
+++ src/app/components/template-details/template-details.component.ts
@@ -5,29 +5,40 @@ import { TemplatesService, ITemplate } from '../../services/templates.service';
 @Component({
   selector: 'app-template-details',
   templateUrl: './template-details.component.html',
-  styleUrls: ['./template-details.component.css']
+  styleUrls: ['./template-details.component.css'],
+  host: {
+    '(document:mouseup)': 'handleMouseUp($event)'
+  }
 })
 
 export class TemplateDetailsComponent implements OnInit {
   pageTitle = "Template Detail";
   errorMessage = "";
-  template: ITemplate | null = null;
+  template: ITemplate | null;
+  selectedText: string = "";
 
   constructor(private route: ActivatedRoute,
     private router: Router,
-    private templatesService: TemplatesService) {
-}
+    private templatesService: TemplatesService) {}
 
-ngOnInit(): void {
-  const param = this.route.snapshot.paramMap.get('id');
+  ngOnInit(): void {
+    const param = this.route.snapshot.paramMap.get('id');
 
-  if (param) {
-    const id = +param;
-    this.templatesService.getTemplate(id).subscribe({
-      next: template => this.template = template,
-      error: (resp) => this.errorMessage = resp
-    })
+    if (param) {
+      const id = +param;
+      this.templatesService.getTemplate(id).subscribe({
+        next: template => this.template = template,
+        error: (resp) => this.errorMessage = resp
+      })
+    }
   }
-}
 
+  handleMouseUp(e: MouseEvent): void {
+    if ((e.target as HTMLElement).closest('.editpanel')) return;
+    if ((e.target as HTMLElement).closest('.editable')) {
+      this.selectedText = document.getSelection().toString();
+      return;
+    }
+    this.selectedText = "";
+  }
 }
