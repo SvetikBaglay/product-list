d374effdb616f7ae270d7b9247eb225014334efb
Svetlana Bahlay <conbka@gmail.com>
2021-05-18 11:58:56 +0300

    added time

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'product-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('product-list');
+    expect(app.pageTitle).toEqual('product-list');
   });
 
   it('should render title', () => {
